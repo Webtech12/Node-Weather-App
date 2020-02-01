a56bebef965160f924126af885bbd26d2de57197
sherwin <sherwinlukes@gmail.com>
2020-02-01 15:48:51 +0500

    forecast function commit

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
 const request  = require('request')
 
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url = "https://api.darksky.net/forecast/826344e2268e49193c82d0cc9ae92b3a/37.8267,-122.4233"
 
-// // requesting and endpoint darksky
-// request(url, (err, res) => {
 
-//     // parsing the json request and storing as an object
-//     const data = JSON.parse(res.body)
-//     // destructuring the (nested) object with ES6 syntax and saving in variables
-//     const { daily : {data : [s] } } = data
 
-//     //const [first] = s
-//     // logging the stored properties in the terminal
-//     console.log(s.summary)
-//     // console.log(`It is currently ${temperature} degrees . There is a ${precipProbability}% chance of rain`)
+// geocode("karachi", (err, data) =>{
+//     console.log(err);
+//     console.log(data);
 // })
-
-
-
-// request(mapurl, (err, res) => {
-//     const parsed = JSON.parse(res.body)
-
-//     const  { features: [index]    } = parsed
-
-//     const lat = index.center[1];
-//     const lon = index.center[0];
-
-
-//     console.log(lat)
-// })
-
-geocode("karachi", (err, data) =>{
+forecast(-75.7088,44.1545, (err, data) =>{
     console.log(err);
     console.log(data);
 })
---
+++ utils/forecast.js
@@ -0,0 +1,25 @@
+const request  = require('request')
+
+const forecast= (lat, lon, callback) => {
+    const url = `https://api.darksky.net/forecast/826344e2268e49193c82d0cc9ae92b3a/${lat},${lon}`
+
+// requesting and endpoint darksky
+    request(url, (err, res) => {
+
+        if (err){
+            callback("can't connect", undefined)
+        }else if(isNaN(lat) || isNaN(lon)){
+            callback("NaN", undefined)
+        }else {
+        // parsing the json request and storing as an object
+        const data = JSON.parse(res.body)
+        // destructuring the (nested) object with ES6 syntax and saving in variables
+        const { daily : {data : [s] } } = data
+        // logging the stored properties in the terminal
+        callback(undefined, s.summary)
+        // callback(undefined, `It is currently ${temperature} degrees . There is a ${precipProbability}% chance of rain`)
+        }
+    })
+}
+
+module.exports = forecast
